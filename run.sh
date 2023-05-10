63e767cf741571ee8f51606beba46f8a6c55acea
John Heo <johnheo@usc.edu>
2023-05-10 16:09:06 -0700

    add more pt files and bash command

--- run.sh
+++ run.sh
@@ -3,12 +3,22 @@
 eval "$(conda shell.bash hook)"
 conda activate 499
 
-# for r in 10 25 50 75; do 
+# sweep ratio values
+for r in 10 25 50 75; do 
+    python main.py \
+        --ratio $r \
+        --n_clusters 20 \
+        --rank_type random \
+        --prune_type common \
+        --dataset mnist
+done
+
+# sweep n_clusters values
 for k in 10 20 30 40 50; do
     python main.py \
         --ratio 25 \
         --n_clusters $k \
         --rank_type random \
         --prune_type common \
-        --dataset fmnist
+        --dataset mnist
 done
